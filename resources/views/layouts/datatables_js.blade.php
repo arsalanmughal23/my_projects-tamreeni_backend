281559cba915eafc45e97e0c426fb22d71db82f9
kashan-mehmood <kashan.mehmood@tekrevol.com>
2023-01-30 21:01:09 +0500

    add "public" in datatable path

--- resources/views/layouts/datatables_js.blade.php
+++ resources/views/layouts/datatables_js.blade.php
@@ -7,7 +7,7 @@
 
 <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.bootstrap4.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.colVis.min.js"></script>
-<script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
+<script src="{{ asset('public/vendor/datatables/buttons.server-side.js') }}"></script>
 <script>
     function submitForm(form) {
         swal({

d8704e42603a762440657da1ceea17cd6ec990fa
Arsalan Mughal Tekrevol <arsalan.mughal@tekrevol.com>
2024-02-27 15:46:56 +0500

    Fix UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -46,7 +46,7 @@ class UserSeeder extends Seeder
             'name' => 'John Doe',
             'email' => 'john.doe@yopmail.com',
             'email_verified_at' => now(),
-            'password' => '123456'
+            'password' => bcrypt('123456')
         ]);
 
         UserDetail::create([
@@ -55,19 +55,19 @@ class UserSeeder extends Seeder
             'last_name' => 'Doe',
             'address' => '123 Main St, City',
             'phone_number' => '123-456-7890',
-            'dob' => '1990-01-01', // Replace with the actual date of birth
-            'image' => 'path/to/image.jpg', // Replace with the actual image path
-            'is_social_login' => 0, // Assuming it's not a social login
-            'language' => 'en', // Replace with the desired language
-            'gender' => 'male', // Replace with the desired gender
-            'current_weight_in_kg' => 70.5, // Replace with the actual current weight
-            'target_weight_in_kg' => 65.0, // Replace with the actual target weight
-            'height_in_m' => 1.75, // Replace with the actual height
-            'goal_id' => 1, // Replace with the desired goal
-            'diet_type_id' => 1, // Replace with the desired diet type
-            'current_weight_unit_id' => 1, // Replace with the desired weight unit
-            'target_weight_unit_id' => 1, // Replace with the desired weight unit
-            'height_unit_id' => 1, // Replace with the desired height unit
+            // 'dob' => '1990-01-01', // Replace with the actual date of birth
+            // 'image' => null, // Replace with the actual image path
+            // 'is_social_login' => 0, // Assuming it's not a social login
+            // 'language' => 'en', // Replace with the desired language
+            // 'gender' => 'male', // Replace with the desired gender
+            // 'current_weight_in_kg' => 70.5, // Replace with the actual current weight
+            // 'target_weight_in_kg' => 65.0, // Replace with the actual target weight
+            // 'height_in_m' => 1.75, // Replace with the actual height
+            // 'goal' => 'lose_weight', // Replace with the desired goal
+            // 'diet_type' => 'traditional', // Replace with the desired diet type
+            // 'current_weight_unit' => 'kg', // Replace with the desired weight unit
+            // 'target_weight_unit' => 'kg', // Replace with the desired weight unit
+            // 'height_unit' => 'm', // Replace with the desired height unit
         ]);
 
     }

8dcc2eeab1c9a16d2bac1302b6018cf591e91f28
Arsalan Mughal Tekrevol <arsalan.mughal@tekrevol.com>
2024-03-04 17:53:22 +0500

    Update MealSeeder for seed more data in meals table

--- database/seeders/MealsSeeder.php
+++ database/seeders/MealsSeeder.php
@@ -19,18 +19,34 @@ class MealsSeeder extends Seeder
                 'diet_type' => 'traditional',
                 'meal_category_id' => 1, // Replace with the ID of a meal category
                 'name' => 'Sample Traditional Meal 1',
-                'image' => 'path/to/image1.jpg',
+                'image' => 'https://placehold.co/400',
                 'calories' => 500,
-                'description' => 'This is a sample traditional meal description.',
+                'description' => 'This is a sample traditional meal 1 description.',
             ],
             [
                 'diet_type' => 'keto',
                 'meal_category_id' => 2, // Replace with the ID of another meal category
                 'name' => 'Sample Keto Meal 1',
-                'image' => 'path/to/image2.jpg',
+                'image' => 'https://placehold.co/400',
                 'calories' => 300,
-                'description' => 'This is a sample keto meal description.',
+                'description' => 'This is a sample keto meal 1 description.',
             ],
+            [
+                'diet_type' => 'keto',
+                'meal_category_id' => 2, // Replace with the ID of another meal category
+                'name' => 'Sample Keto Meal 2',
+                'image' => 'https://placehold.co/400',
+                'calories' => 400,
+                'description' => 'This is a sample keto meal 2 description.',
+            ],
+            [
+                'diet_type' => 'traditional',
+                'meal_category_id' => 1, // Replace with the ID of a meal category
+                'name' => 'Sample Traditional Meal 2',
+                'image' => 'https://placehold.co/400',
+                'calories' => 200,
+                'description' => 'This is a sample traditional meal 2 description.',
+            ]
         ];
 
         DB::table('meals')->insert($meals);

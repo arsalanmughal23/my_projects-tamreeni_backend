b6214c8102b363f98548e97bb407b020ccab7c9e
Arsalan Mughal Tekrevol <arsalan.mughal@tekrevol.com>
2024-03-11 18:24:34 +0500

    SubmitAnswerApiRequest Extend with BaseApiRequest due to return relavant error message response

--- app/Http/Requests/API/SubmitAnswersAPIRequest.php
+++ app/Http/Requests/API/SubmitAnswersAPIRequest.php
@@ -4,9 +4,8 @@ namespace App\Http\Requests\API;
 
 use App\Models\Constant;
 use App\Models\Question;
-use Illuminate\Foundation\Http\FormRequest;
 
-class SubmitAnswersAPIRequest extends FormRequest
+class SubmitAnswersAPIRequest extends BaseAPIRequest
 {
     /**
      * Determine if the user is authorized to make this request.

f1693c045bebb02189de3a1ca015d738083d9473
Shoaib Ansari <shoaib.ansari@tekrevol.com>
2024-03-20 22:20:12 +0500

    +event module with multiple lang

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        App::setLocale('en');
+        Schema::defaultStringLength(191);
+        $this->app['request']->server->set('HTTPS', true);
     }
 }

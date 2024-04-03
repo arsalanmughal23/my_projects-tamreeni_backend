d1952bdf26d38f6502c822e4cc691f9db01af5c6
faiz <faiz@tekrevol.com>
2024-04-03 14:49:09 +0500

    + migrations added

---
+++ database/migrations/2024_04_03_090447_create_nutrition_plan_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNutritionPlanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nutrition_plans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name', 255)->nullable(false);
+            $table->date('start_date')->nullable(false);
+            $table->date('end_date')->nullable(false);
+            $table->integer('status')->nullable(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('meal_plans', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2024_04_03_090748_create_nutrition_plan_day_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNutritionPlanDayTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nutrition_plan_days', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('nutrition_plan_id');
+            $table->foreign('nutrition_plan_id')->references('id')->on('nutrition_plans')->onDelete('cascade');
+            $table->string('name', 255)->nullable(false);
+            $table->integer('status')->nullable(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('nutrition_plan_days', function (Blueprint $table) {
+            //
+        });
+    }
+}

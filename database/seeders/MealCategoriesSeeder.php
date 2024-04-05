a7b28b9fe0ec5574322b3f5834b19c442347f243
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-05 23:50:21 +0500

    Fix MealCategory (Migration, Seeder, Models)

--- database/seeders/MealCategoriesSeeder.php
+++ database/seeders/MealCategoriesSeeder.php
@@ -15,9 +15,9 @@ class MealCategoriesSeeder extends Seeder
     public function run()
     {
         $categories = [
-            [ 'slug' => 'breakfast', 'title' => 'Breakfast' ],
-            [ 'slug' => 'lunch', 'title' => 'Lunch' ],
-            [ 'slug' => 'dinner', 'title' => 'Dinner' ],
+            [ 'diet_type' => 'traditional', 'name' => json_encode(['en'=>'Vegetarian', 'ar'=>'نباتي']) ],
+            [ 'diet_type' => 'traditional', 'name' => json_encode(['en'=>'Lactose Free', 'ar'=>'خالي من اللاكتوز']) ],
+            [ 'diet_type' => 'keto', 'name' => json_encode(['en'=>'Gluten Free', 'ar'=>'خالي من الغلوتين']) ],
         ];
 
         DB::table('meal_categories')->insert($categories);

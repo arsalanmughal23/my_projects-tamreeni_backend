215fcec05ac2c6a877aab7c6c79049f8d362d51b
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-08 11:06:51 +0500

    Fix Migration & Seeders files   - Setting Migration   - DatabaseSeeder   - RoleSeeder   - UserSeeder   - SettingSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            SettingSeeder::class,
             UserSeeder::class,
-            RoleSeeder::class,
             MenuSeeder::class,
             ConstantSeeder::class,
             WellnessTipsSeeder::class,

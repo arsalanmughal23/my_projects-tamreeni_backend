467661badabe2c47650bab23e0b58e5bfa89731d
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-18 00:43:17 +0500

    Add New En & Ar Language File for messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'bmi_description' => 'A BMI of :bmi falls within the `normal weight` category, which typically ranges between 18.5 to 24.5. It suggests that a person is at a healthy weight in proportion to their height.'
+];

c68d3080f92eb5c0ec4af76a9f4825e979de7718
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-26 20:50:55 +0500

    Implement Multi-Lingual Feature in Test-Notification Api

--- app/Http/Controllers/API/NotificationAPIController.php
+++ app/Http/Controllers/API/NotificationAPIController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Constants\NotificationServiceTemplateNames;
 use App\Http\Controllers\AppBaseController;
-use App\Http\Controllers\Controller;
 use App\Models\Appointment;
-use App\Models\NutritionPlan;
 use App\Models\NutritionPlanDayMeal;
 use App\Models\User;
 use Error;
@@ -67,25 +65,39 @@ class NotificationAPIController extends AppBaseController
         }
     }
 
-    public static function sendNotification(User $user, $NOTIFICATION_TYPE, $refId)
-    {
-        $userDeviceTokens = $user->devices->pluck('device_token');
+    public static function sendNotification(User $user, $NOTIFICATION_TYPE, $refId, $title = [], $message = [])
+    {        
+        if (!is_array($title) || count($title) < 2)
+            $title = ['Title', 'عنوان'];
+        
+        if (!is_array($message) || count($message) < 2)
+            $message = ['Message', 'رسالة'];
 
+        $userDeviceTokens = $user->devices->pluck('device_token');
         $userWithDeviceTokens = [
             "_id" => strval($user->id),
             "device_tokens" => $userDeviceTokens
         ];
-        $notificationRequest = new Request([
+        $payload = [
             "users" => [
                 $userWithDeviceTokens
             ],
             "template_name" => $NOTIFICATION_TYPE,
             "data" => [
+                "title" => json_encode([
+                    'en' => is_string($title[0]) ? $title[0] : 'title',
+                    'ar' => is_string($title[1]) ? $title[1] : 'عنوان',
+                ]),
+                "message" => json_encode([
+                    'en' => is_string($message[0]) ? $message[0] : 'body',
+                    'ar' => is_string($message[1]) ? $message[1] : 'هيئة',
+                ]),
                 "ref_id" => $refId,
                 "notification_type" => $NOTIFICATION_TYPE,
                 "user_name" => $user->name
             ]
-        ]);
+        ];
+        $notificationRequest = new Request($payload);
         $sendNotificationResponse = self::post($notificationRequest, 'post.messaging');
 
         if (!$sendNotificationResponse['status'])
@@ -105,10 +117,16 @@ class NotificationAPIController extends AppBaseController
             $notificationResponse   = null;
             $notificationRefModule  = null;
             $notificationType       = null;
+            $title = [];
+            $message = [];
 
             switch ($request->type) {
                 case NotificationServiceTemplateNames::APPOINTMENT:
                     $notificationType = NotificationServiceTemplateNames::APPOINTMENT;
+                    // TODO :: Make Title & Body multi-lingual content through using singleton
+                    // Multi-Language title & body must have en & ar langauges
+                    $title = ['Appointment Reminder', 'منبه مواعيد'];
+                    $message = ['This is a reminder for your appointment', 'هذا تذكير لموعدك'];
                     $notificationRefModule = Appointment::where('customer_id', $user->id)->first();
                     if (!$notificationRefModule)
                         throw new Error('No Appointment Found');
@@ -116,6 +134,9 @@ class NotificationAPIController extends AppBaseController
 
                 case NotificationServiceTemplateNames::MEAL:
                     $notificationType = NotificationServiceTemplateNames::MEAL;
+                    $title = ['Meal Reminder', 'تذكير وجبة'];
+                    $message = ['This is a reminder for your meal', 'هذا تذكير لوجبتك'];
+
                     $nutritionPlanId = $user->details?->current_nutrition_plan_id;
                     if (!$nutritionPlanId)
                         throw new Error('Nutrition Plan is not available');
@@ -132,7 +153,7 @@ class NotificationAPIController extends AppBaseController
                     throw new Error('Other Test Notifications is not available');
             }
 
-            $notificationResponse = NotificationAPIController::sendNotification($user, $notificationType, $notificationRefModule->id);
+            $notificationResponse = NotificationAPIController::sendNotification($user, $notificationType, $notificationRefModule->id, $title, $message);
             return $this->sendResponse($notificationRefModule, $notificationResponse['message']);
         } catch (\Error $e) {
             return $this->sendError($e->getMessage(), 403);

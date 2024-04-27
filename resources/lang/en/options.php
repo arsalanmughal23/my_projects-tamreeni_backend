fcabc8a766a481b2c91cc453056f65ea9d523dbc
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-27 13:29:14 +0500

    Make English & Arabic Languages for Questionnair Module (Questions & Options)

---
+++ resources/lang/en/options.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+    'lose_weight'   => 'Lose Weight',
+    'gain_weight'   => 'Gain Weight',
+    'build_muscle'  => 'Build Muscle',
+    'get_fit'       => 'Get Fit',
+
+    'male'      => 'Male',
+    'female'    => 'Female',
+
+    '1_to_2_days_a_week'    => '1 to 2 days a week',
+    '2_to_4_days_a_week'    => '2 to 4 days a week',
+    '5_days_a_week'         => '5 days a week',
+
+    '5_to_10_mins_a_day'    => '5 to 10 mins a day',
+    '10_to_15_mins_a_day'   => '10 to 15 mins a day',
+    '15_to_20_mins_a_day'   => '15 to 20 mins a day',
+
+    'home'  => 'Home',
+    'gym'   => 'Gym',
+
+    'all_equipments'        => 'All equipments',
+    'free_weight'           => 'Free weight',
+    'machines'              => 'Machines',
+    'no_equipment_at_all'   => 'No equipment at all',
+
+    'yes'   => 'Yes',
+    'no'    => 'No',
+
+    '30_mins'           => '30 mins',
+    '45_mins'           => '45 mins',
+    '1_hour'            => '1 hour',
+    'more_than_1_hour'  => 'more than 1 hour',
+
+    'back'  => 'Back',
+    'arms'  => 'Arms',
+    'chest' => 'Chest',
+    'abs'   => 'Abs',
+    'legs'  => 'Legs',
+
+    'traditional'   => 'Traditional',
+    'keto'          => 'Keto',
+
+    'egg'       => 'Egg',
+    'fish'      => 'Fish',
+    'shrimp'    => 'Shrimp',
+    'dairy'     => 'Dairy',
+    'vegies'    => 'Vegies',
+    'sea_food'  => 'Sea Food',
+
+    'beginner'      => 'Beginner',
+    'intermediate'  => 'Intermediate',
+    'advanced'      => 'Advanced',
+
+    // 'yes'   => 'Yes',
+    // 'no'    => 'No',
+
+    '2000_4000_steps'   => '2000 4000 steps',
+    '5000_7000_steps'   => '5000 7000 steps',
+    '7000_10000_steps'  => '7000 10000 steps',
+    '10000_plus_steps'  => '10000 plus steps',
+];

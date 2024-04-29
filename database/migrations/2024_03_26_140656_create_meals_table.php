9d9117f537a7770eb9e995af4dad69988464179c
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-29 13:48:31 +0500

    Fix Meal Migration for created_at & updated_at with default values

--- database/migrations/2024_03_26_140656_create_meals_table.php
+++ database/migrations/2024_03_26_140656_create_meals_table.php
@@ -32,7 +32,8 @@ class CreateMealsTable extends Migration
 
             $table->text('description')->nullable();
             $table->softDeletes();
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 

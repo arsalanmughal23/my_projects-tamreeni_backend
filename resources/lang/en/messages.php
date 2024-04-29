5980a88de1b73e0797ed0ff96d623acba4e56327
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-29 13:49:04 +0500

    Add Languages For Exercises (Title & Description)

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
+    'exercise_title' => 'Exercise :number',
+    'exercise_description' => 'Description of Exercise :number',
     'bmi_description' => 'A BMI of :bmi falls within the `normal weight` category, which typically ranges between 18.5 to 24.5. It suggests that a person is at a healthy weight in proportion to their height.'
 ];

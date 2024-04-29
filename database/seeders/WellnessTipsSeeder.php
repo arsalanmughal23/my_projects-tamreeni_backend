e3b900185ebb7301729da0ae3096b829e09292bd
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-29 14:49:08 +0500

    Modify WellnessTips Module for Multi-Lingual feature with Seeders (Seeding Data)    - Make WellnessTipResource    - Implement WellnessTipResource in List & Show Api for Multi-Lingual    - Make WellnessTip Model Translatable    - Manage WellnessTipsSeeder for insert seeding data according to multi-lingual format    - Add Multi-Lingual Languages for WellnessTips Module

--- database/seeders/WellnessTipsSeeder.php
+++ database/seeders/WellnessTipsSeeder.php
@@ -14,23 +14,35 @@ class WellnessTipsSeeder extends Seeder
      */
     public function run()
     {
+        WellnessTip::truncate();
+        
         // Create wellness tips
         $wellnessTips = [
             [
-                'title' => 'Tip 1: Healthy Eating',
-                'content' => 'Include a variety of fruits, vegetables, and whole grains in your diet for better health.',
+                'title' => json_encode([
+                    'en' => __('messages.wellness_tip.healthy_eating.title', ['number' => 1], 'en'),
+                    'ar' => __('messages.wellness_tip.healthy_eating.title', ['number' => 1], 'ar'),
+                ]),
+                'content' => json_encode([
+                    'en' => __('messages.wellness_tip.healthy_eating.content', [], 'en'),
+                    'ar' => __('messages.wellness_tip.healthy_eating.content', [], 'ar')
+                ]),
                 'cover' => 'path/to/cover1.jpg', // Replace with the actual cover image path
             ],
             [
-                'title' => 'Tip 2: Stay Hydrated',
-                'content' => 'Drink an adequate amount of water throughout the day to stay hydrated and support bodily functions.',
+                'title' => json_encode([
+                    'en' => __('messages.wellness_tip.stay_hydrated.title', ['number' => 2], 'en'),
+                    'ar' => __('messages.wellness_tip.stay_hydrated.title', ['number' => 2], 'ar')
+                ]),
+                'content' => json_encode([
+                    'en' => __('messages.wellness_tip.stay_hydrated.content', [], 'en'),
+                    'ar' => __('messages.wellness_tip.stay_hydrated.content', [], 'ar')
+                ]),
                 'cover' => 'path/to/cover2.jpg', // Replace with the actual cover image path
             ],
             // Add more wellness tips as needed
         ];
 
-        foreach ($wellnessTips as $tip) {
-            WellnessTip::create($tip);
-        }
+        WellnessTip::insert($wellnessTips);
     }
 }

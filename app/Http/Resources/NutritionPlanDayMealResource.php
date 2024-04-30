7481ccd46998daf634a1bf5d9f57280b38e00dec
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-04-30 17:21:26 +0500

    Fix & Modify Resources for (NutritionPlanDayMeal & NutritionPlan) Modules

--- app/Http/Resources/NutritionPlanDayMealResource.php
+++ app/Http/Resources/NutritionPlanDayMealResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use App\Models\NutritionPlanDayMeal;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class NutritionPlanDayMealResource extends JsonResource
 {
@@ -18,37 +19,44 @@ class NutritionPlanDayMealResource extends JsonResource
         return [
             "id"                    => $this->id,
             "nutrition_plan_day_id" => $this->nutrition_plan_day_id,
-            "meal_id"               => $this->meal_id,
-            "meal_type_id"          => $this->meal_type_id,
-            "name"                  => $this->meal->name,
-            "diet_type"             => $this->meal->diet_type,
-            "calories"              => $this->calories,
-            "carbs"                 => $this->carbs,
-            "fats"                  => $this->fats,
-            "protein"               => $this->protein,
-            "image"                 => $this->meal->image,
-            "status"                => $this->status,
-            "created_at"            => $this->created_at,
-            "meal_type"             => $this->mealType->name
+            "meal_id"       => $this->meal_id,
+            "meal_type_id"  => $this->meal_type_id,
+            "name"          => $this->name,
+            "diet_type"     => $this->diet_type,
+            "calories"      => $this->calories,
+            "carbs"         => $this->carbs,
+            "fats"          => $this->fats,
+            "protein"       => $this->protein,
+            "image"         => $this->meal?->image,
+            "status"        => $this->status,
+            "created_at"    => $this->created_at,
+            "meal_type"     => new MealTypeResource($this->mealType)
         ];
     }
 
-    
-    public static function toObject(NutritionPlanDayMeal $data)
+    public static function collection($resource)
     {
-        return [
-            'nutrition_plan_day_id' => $data->nutrition_plan_day_id,
-            'meal_id'               => $data->meal_id,
-            'name'                  => $data->name,
-            'diet_type'             => $data->diet_type,
-            'calories'              => $data->calories,
-            'carbs'                 => $data->carbs,
-            'fats'                  => $data->fats,
-            'protein'               => $data->protein,
-            'status'                => $data->status,
-            // 'meal_type_id'          => $data->meal_type_id,
-            // 'image'                 => $data->image,
-            // 'meal_type'             => $data->meal_type
-        ];
+        if ($resource instanceof LengthAwarePaginator) {
+            return [
+                'data'  => parent::collection($resource),
+                'links' => [
+                    'first' => $resource->url(1),
+                    'last'  => $resource->url($resource->lastPage()),
+                    'prev'  => $resource->previousPageUrl(),
+                    'next'  => $resource->nextPageUrl(),
+                ],
+                'meta'  => [
+                    'current_page' => $resource->currentPage(),
+                    'from'         => $resource->firstItem(),
+                    'last_page'    => $resource->lastPage(),
+                    'path'         => $resource->path(),
+                    'per_page'     => $resource->perPage(),
+                    'to'           => $resource->lastItem(),
+                    'total'        => $resource->total(),
+                ],
+            ];
+        }
+
+        return parent::collection($resource);
     }
 }

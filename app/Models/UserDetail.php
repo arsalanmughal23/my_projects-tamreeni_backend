3e72f28925471083f82c1e6e3fcfa812c76d4692
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-05-10 08:57:45 +0500

    Add New & Re-arrange Questions Fields in Models/UserDetail

--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -74,12 +74,20 @@ class UserDetail extends Model
         'reach_goal_target_date',
 
         'body_parts',
+        
+        'physically_active',
+        
+        'level',
+        'squat__one_rep_max_in_kg',
+        'deadlift__one_rep_max_in_kg',
+        'bench__one_rep_max_in_kg',
+        'overhead__one_rep_max_in_kg',
+        
+        'health_status',
+        'daily_steps_taken',
+        
         'diet_type',
         'food_preferences',
-
-        'level',
-        'daily_steps_taken',
-        'health_status',
 
         'calories',
 

9fed906b03bfe3810dfb3b30968ab2388a0b5966
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-05-10 17:33:00 +0500

    Test Appointment API v2

--- app/Http/Controllers/PayTabsController.php
+++ app/Http/Controllers/PayTabsController.php
@@ -92,6 +92,7 @@ class PayTabsController extends AppBaseController
             'Content-type'  => 'application/json'
         ])->post('https://secure.paytabs.sa/payment/request', $fields);
         // TODO :: Need to remove logger
+        \Illuminate\Support\Facades\Log::info('test appointment api: paytab_details: '.json_encode([env('PAYTABS_SERVER_URL'), env('PAYTABS_PROFILE_ID')]));
         \Illuminate\Support\Facades\Log::info('test appointment api: fields: '.json_encode($fields));
         \Illuminate\Support\Facades\Log::info('test appointment api: response: '.json_encode($response->json()));
         return $response->json();

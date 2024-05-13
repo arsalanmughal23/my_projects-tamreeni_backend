fdc8c112728c6e116cfdf79737ff000f8667f2ee
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-05-13 23:28:31 +0500

    Modify MealCategory migration & model    - Add Slug Field in Models/MealCategory    - Add slug field and modify name filed in meal_categories migration

--- database/migrations/2024_01_30_140505_create_meal_categories_table.php
+++ database/migrations/2024_01_30_140505_create_meal_categories_table.php
@@ -16,7 +16,8 @@ class CreateMealCategoriesTable extends Migration
         Schema::create('meal_categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('diet_type')->comment('traditional', 'keto')->nullable(); // Assuming const_diet_types is an enum
-            $table->string('name')->comment('eggs', 'shrimp', 'veggies')->nullable(); // food_preferences
+            $table->string('slug')->comment('eggs','fish','shrimp','dairy','veggies','sea_food'); // food_preferences
+            $table->string('name')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });

eb4d5df000a4c6a836a9d9de46bde6ac2a08f20f
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-05-14 23:03:53 +0500

    Modify (Improve) ExerciseSeeder    - Assign Random BodyPart to Created Exercise    - Manage & Assign Random ExerciseEquipments to Created Exercise

--- database/seeders/ExerciseSeeder.php
+++ database/seeders/ExerciseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use DB;
+use App\Models\BodyPart;
 use App\Models\Exercise;
+use App\Models\ExerciseEquipment;
 use Illuminate\Database\Seeder;
 
 class ExerciseSeeder extends Seeder
@@ -15,12 +16,12 @@ class ExerciseSeeder extends Seeder
      */
     public function run()
     {
+        $bodyPartIds = BodyPart::pluck('id');
         $exercises = [
             // CATEGORY_MAJOR_LIFT
             [
                 'user_id' => 1,
-                'body_part_id' => 1,
-                'name' => json_encode(['en' => __('messages.exercise_title', ['number' => 1], 'en'), 'ar' => __('messages.exercise_title', ['number' => 1], 'ar')]),
+                'name' => ['en' => __('messages.exercise_title', ['number' => 1], 'en'), 'ar' => __('messages.exercise_title', ['number' => 1], 'ar')],
                 'exercise_category_name' => Exercise::CATEGORY_MAJOR_LIFT,
                 'exercise_type_name' => Exercise::TYPE_BENCH,
                 'duration_in_m' => 30,
@@ -29,14 +30,13 @@ class ExerciseSeeder extends Seeder
                 'burn_calories' => 150,
                 'image' => 'https://tamreeni-backend.s3.amazonaws.com/Exercises/01.jpg',
                 'video' => 'https://tamreeni-backend.s3.amazonaws.com/ExerciseVideos/01.mp4',
-                'description' => json_encode(['en' => __('messages.exercise_description', ['number' => 1], 'en'), 'ar' => __('messages.exercise_description', ['number' => 1], 'ar')]),
+                'description' => ['en' => __('messages.exercise_description', ['number' => 1], 'en'), 'ar' => __('messages.exercise_description', ['number' => 1], 'ar')],
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'user_id' => 1,
-                'body_part_id' => 2,
-                'name' => json_encode(['en' => __('messages.exercise_title', ['number' => 2], 'en'), 'ar' => __('messages.exercise_title', ['number' => 2], 'ar')]),
+                'name' => ['en' => __('messages.exercise_title', ['number' => 2], 'en'), 'ar' => __('messages.exercise_title', ['number' => 2], 'ar')],
                 'exercise_category_name' => Exercise::CATEGORY_MAJOR_LIFT,
                 'exercise_type_name' => Exercise::TYPE_DEADLIFT,
                 'duration_in_m' => 20,
@@ -45,14 +45,13 @@ class ExerciseSeeder extends Seeder
                 'burn_calories' => 120,
                 'image' => 'https://tamreeni-backend.s3.amazonaws.com/Exercises/02.jpg',
                 'video' => 'https://tamreeni-backend.s3.amazonaws.com/ExerciseVideos/02.mp4',
-                'description' => json_encode(['en' => __('messages.exercise_description', ['number' => 2], 'en'), 'ar' => __('messages.exercise_description', ['number' => 2], 'ar')]),
+                'description' => ['en' => __('messages.exercise_description', ['number' => 2], 'en'), 'ar' => __('messages.exercise_description', ['number' => 2], 'ar')],
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'user_id' => 2,
-                'body_part_id' => 1,
-                'name' => json_encode(['en' => __('messages.exercise_title', ['number' => 3], 'en'), 'ar' => __('messages.exercise_title', ['number' => 3], 'ar')]),
+                'name' => ['en' => __('messages.exercise_title', ['number' => 3], 'en'), 'ar' => __('messages.exercise_title', ['number' => 3], 'ar')],
                 'exercise_category_name' => Exercise::CATEGORY_MAJOR_LIFT,
                 'exercise_type_name' => Exercise::TYPE_OVERHEAD,
                 'duration_in_m' => 40,
@@ -61,14 +60,13 @@ class ExerciseSeeder extends Seeder
                 'burn_calories' => 180,
                 'image' => 'https://tamreeni-backend.s3.amazonaws.com/Exercises/03.jpg',
                 'video' => 'https://tamreeni-backend.s3.amazonaws.com/ExerciseVideos/03.mp4',
-                'description' => json_encode(['en' => __('messages.exercise_description', ['number' => 3], 'en'), 'ar' => __('messages.exercise_description', ['number' => 3], 'ar')]),
+                'description' => ['en' => __('messages.exercise_description', ['number' => 3], 'en'), 'ar' => __('messages.exercise_description', ['number' => 3], 'ar')],
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'user_id' => 2,
-                'body_part_id' => 1,
-                'name' => json_encode(['en' => __('messages.exercise_title', ['number' => 4], 'en'), 'ar' => __('messages.exercise_title', ['number' => 4], 'ar')]),
+                'name' => ['en' => __('messages.exercise_title', ['number' => 4], 'en'), 'ar' => __('messages.exercise_title', ['number' => 4], 'ar')],
                 'exercise_category_name' => Exercise::CATEGORY_MAJOR_LIFT,
                 'exercise_type_name' => Exercise::TYPE_SQUAT,
                 'duration_in_m' => 40,
@@ -77,15 +75,14 @@ class ExerciseSeeder extends Seeder
                 'burn_calories' => 180,
                 'image' => 'https://tamreeni-backend.s3.amazonaws.com/Exercises/04.jpg',
                 'video' => 'https://tamreeni-backend.s3.amazonaws.com/ExerciseVideos/04.mp4',
-                'description' => json_encode(['en' => __('messages.exercise_description', ['number' => 4], 'en'), 'ar' => __('messages.exercise_description', ['number' => 4], 'ar')]),
+                'description' => ['en' => __('messages.exercise_description', ['number' => 4], 'en'), 'ar' => __('messages.exercise_description', ['number' => 4], 'ar')],
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             // CATEGORY_ACCESSORY_MOVEMENT
             [
                 'user_id' => 2,
-                'body_part_id' => 1,
-                'name' => json_encode(['en' => __('messages.exercise_title', ['number' => 5], 'en'), 'ar' => __('messages.exercise_title', ['number' => 5], 'ar')]),
+                'name' => ['en' => __('messages.exercise_title', ['number' => 5], 'en'), 'ar' => __('messages.exercise_title', ['number' => 5], 'ar')],
                 'exercise_category_name' => Exercise::CATEGORY_ACCESSORY_MOVEMENT,
                 'exercise_type_name' => null,
                 'duration_in_m' => 40,
@@ -94,14 +91,13 @@ class ExerciseSeeder extends Seeder
                 'burn_calories' => 180,
                 'image' => 'https://tamreeni-backend.s3.amazonaws.com/Exercises/04.jpg',
                 'video' => 'https://tamreeni-backend.s3.amazonaws.com/ExerciseVideos/04.mp4',
-                'description' => json_encode(['en' => __('messages.exercise_description', ['number' => 5], 'en'), 'ar' => __('messages.exercise_description', ['number' => 5], 'ar')]),
+                'description' => ['en' => __('messages.exercise_description', ['number' => 5], 'en'), 'ar' => __('messages.exercise_description', ['number' => 5], 'ar')],
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'user_id' => 2,
-                'body_part_id' => 1,
-                'name' => json_encode(['en' => __('messages.exercise_title', ['number' => 6], 'en'), 'ar' => __('messages.exercise_title', ['number' => 6], 'ar')]),
+                'name' => ['en' => __('messages.exercise_title', ['number' => 6], 'en'), 'ar' => __('messages.exercise_title', ['number' => 6], 'ar')],
                 'exercise_category_name' => Exercise::CATEGORY_ACCESSORY_MOVEMENT,
                 'exercise_type_name' => null,
                 'duration_in_m' => 40,
@@ -110,15 +106,14 @@ class ExerciseSeeder extends Seeder
                 'burn_calories' => 180,
                 'image' => 'https://tamreeni-backend.s3.amazonaws.com/Exercises/04.jpg',
                 'video' => 'https://tamreeni-backend.s3.amazonaws.com/ExerciseVideos/04.mp4',
-                'description' => json_encode(['en' => __('messages.exercise_description', ['number' => 6], 'en'), 'ar' => __('messages.exercise_description', ['number' => 6], 'ar')]),
+                'description' => ['en' => __('messages.exercise_description', ['number' => 6], 'en'), 'ar' => __('messages.exercise_description', ['number' => 6], 'ar')],
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             // CATEGORY_SINGLE_JOINT
             [
                 'user_id' => 2,
-                'body_part_id' => 1,
-                'name' => json_encode(['en' => __('messages.exercise_title', ['number' => 7], 'en'), 'ar' => __('messages.exercise_title', ['number' => 7], 'ar')]),
+                'name' => ['en' => __('messages.exercise_title', ['number' => 7], 'en'), 'ar' => __('messages.exercise_title', ['number' => 7], 'ar')],
                 'exercise_category_name' => Exercise::CATEGORY_SINGLE_JOINT,
                 'exercise_type_name' => null,
                 'duration_in_m' => 40,
@@ -127,15 +122,14 @@ class ExerciseSeeder extends Seeder
                 'burn_calories' => 180,
                 'image' => 'https://tamreeni-backend.s3.amazonaws.com/Exercises/04.jpg',
                 'video' => 'https://tamreeni-backend.s3.amazonaws.com/ExerciseVideos/04.mp4',
-                'description' => json_encode(['en' => __('messages.exercise_description', ['number' => 7], 'en'), 'ar' => __('messages.exercise_description', ['number' => 7], 'ar')]),
+                'description' => ['en' => __('messages.exercise_description', ['number' => 7], 'en'), 'ar' => __('messages.exercise_description', ['number' => 7], 'ar')],
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             // CATEGORY_MULTI_JOINT
             [
                 'user_id' => 2,
-                'body_part_id' => 1,
-                'name' => json_encode(['en' => __('messages.exercise_title', ['number' => 8], 'en'), 'ar' => __('messages.exercise_title', ['number' => 8], 'ar')]),
+                'name' => ['en' => __('messages.exercise_title', ['number' => 8], 'en'), 'ar' => __('messages.exercise_title', ['number' => 8], 'ar')],
                 'exercise_category_name' => Exercise::CATEGORY_MULTI_JOINT,
                 'exercise_type_name' => null,
                 'duration_in_m' => 40,
@@ -144,15 +138,14 @@ class ExerciseSeeder extends Seeder
                 'burn_calories' => 180,
                 'image' => 'https://tamreeni-backend.s3.amazonaws.com/Exercises/04.jpg',
                 'video' => 'https://tamreeni-backend.s3.amazonaws.com/ExerciseVideos/04.mp4',
-                'description' => json_encode(['en' => __('messages.exercise_description', ['number' => 8], 'en'), 'ar' => __('messages.exercise_description', ['number' => 8], 'ar')]),
+                'description' => ['en' => __('messages.exercise_description', ['number' => 8], 'en'), 'ar' => __('messages.exercise_description', ['number' => 8], 'ar')],
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             // CATEGORY_CARDIO
             [
                 'user_id' => 2,
-                'body_part_id' => 1,
-                'name' => json_encode(['en' => __('messages.exercise_title', ['number' => 9], 'en'), 'ar' => __('messages.exercise_title', ['number' => 9], 'ar')]),
+                'name' => ['en' => __('messages.exercise_title', ['number' => 9], 'en'), 'ar' => __('messages.exercise_title', ['number' => 9], 'ar')],
                 'exercise_category_name' => Exercise::CATEGORY_CARDIO,
                 'exercise_type_name' => null,
                 'duration_in_m' => 40,
@@ -161,13 +154,24 @@ class ExerciseSeeder extends Seeder
                 'burn_calories' => 180,
                 'image' => 'https://tamreeni-backend.s3.amazonaws.com/Exercises/04.jpg',
                 'video' => 'https://tamreeni-backend.s3.amazonaws.com/ExerciseVideos/04.mp4',
-                'description' => json_encode(['en' => __('messages.exercise_description', ['number' => 9], 'en'), 'ar' => __('messages.exercise_description', ['number' => 9], 'ar')]),
+                'description' => ['en' => __('messages.exercise_description', ['number' => 9], 'en'), 'ar' => __('messages.exercise_description', ['number' => 9], 'ar')],
                 'created_at' => now(),
                 'updated_at' => now(),
             ]
         ];
 
         // Insert the records into the database
-        Exercise::insert($exercises);
+        $exerciseEquipmentIds = ExerciseEquipment::pluck('id');
+        foreach($exercises as $key => $exercise) {
+            $exercise['body_part_id'] = $bodyPartIds->random();
+            $newExercise = Exercise::create($exercise);
+
+            if($key < 2){
+                $newExercise->equipment()->attach($exerciseEquipmentIds);
+            } else {
+                if(!($key % 2 == 0))
+                    $newExercise->equipment()->attach($exerciseEquipmentIds->random());
+            }
+        }
     }
 }

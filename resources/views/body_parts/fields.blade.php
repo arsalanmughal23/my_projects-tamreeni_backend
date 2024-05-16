05a3145d53d31620aa09f581854b7b7e67b1eb95
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-05-16 20:12:29 +0500

    Update Regex Pattern for Admin Panel Modules    - Regex should accept selected special characters like (+ & -) for both language English & Arabic

--- resources/views/body_parts/fields.blade.php
+++ resources/views/body_parts/fields.blade.php
@@ -1,13 +1,13 @@
 <!-- Name Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('name', 'Name (English):', ['class'=>'required']) !!}
-    {!! Form::text('name[en]', isset($bodyPart)?$bodyPart->getTranslation('name', 'en'):null, ['class' => 'form-control','maxlength' => 70, 'required', 'pattern'=>'[a-zA-Z0-9_.\s]{0,70}']) !!}
+    {!! Form::text('name[en]', isset($bodyPart)?$bodyPart->getTranslation('name', 'en'):null, ['class' => 'form-control','maxlength' => 70, 'required', 'pattern'=>'[a-zA-Z0-9_.\s+\-]{0,70}']) !!}
 </div>
 
 <!-- Name Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('name', 'Name (Arabic):', ['class'=>'required']) !!}
-    {!! Form::text('name[ar]', isset($bodyPart)?$bodyPart->getTranslation('name', 'ar'):null, ['class' => 'form-control','maxlength' => 70, 'required', 'dir'=>'rtl', 'pattern'=>'[ا-ي0-9_.\s]{0,70}']) !!}
+    {!! Form::text('name[ar]', isset($bodyPart)?$bodyPart->getTranslation('name', 'ar'):null, ['class' => 'form-control','maxlength' => 70, 'required', 'dir'=>'rtl', 'pattern'=>'[ا-ي0-9_.\s+-]{0,70}']) !!}
 </div>
 
 <!-- Image Field -->

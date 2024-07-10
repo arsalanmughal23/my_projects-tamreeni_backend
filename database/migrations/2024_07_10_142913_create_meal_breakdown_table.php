f437437190060122798d1e12ec61780addce2342
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-07-10 21:29:56 +0500

    Make migration for meal_breakdown module    - for traditional_meal_breakdown & keto_meal_breakdown

---
+++ database/migrations/2024_07_10_142913_create_meal_breakdown_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealBreakdownTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meal_breakdown', function (Blueprint $table) {
+            $table->id();
+            $table->enum('diet_type', ['traditional', 'keto']);
+            $table->bigInteger('total_calories');
+            $table->unsignedBigInteger('breakfast_units')->default(0);
+            $table->unsignedBigInteger('lunch_units')->default(0);
+            $table->unsignedBigInteger('dinner_units')->default(0);
+            $table->unsignedBigInteger('fruit_units')->default(0);
+            $table->unsignedBigInteger('snack_units')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meal_breakdown');
+    }
+}

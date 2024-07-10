9240f5e9e3e0be8a8dcb038806f91a77ec35ec3d
Arsalan Mughal <arsalanmughal23@gmail.com>
2024-07-10 22:00:00 +0500

    Refactor meal_breakdown migration for plural name

--- database/migrations/2024_07_10_165640_create_meal_breakdowns_table.php
+++ database/migrations/2024_07_10_165640_create_meal_breakdowns_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMealBreakdownTable extends Migration
+class CreateMealBreakdownsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateMealBreakdownTable extends Migration
      */
     public function up()
     {
-        Schema::create('meal_breakdown', function (Blueprint $table) {
+        Schema::create('meal_breakdowns', function (Blueprint $table) {
             $table->id();
             $table->enum('diet_type', ['traditional', 'keto']);
             $table->bigInteger('total_calories');
@@ -33,6 +33,6 @@ class CreateMealBreakdownTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('meal_breakdown');
+        Schema::dropIfExists('meal_breakdowns');
     }
 }

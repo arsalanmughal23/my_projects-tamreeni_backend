5f463cecc8944277356a1fab74593778bde7e8b9
Arsalan Mughal <arsalan.mughal@tekrevol.com>
2024-07-24 21:55:21 +0500

    Update Migration files

--- database/migrations/2024_07_10_165640_create_meal_breakdowns_table.php
+++ database/migrations/2024_07_10_165640_create_meal_breakdowns_table.php
@@ -13,20 +13,22 @@ class CreateMealBreakdownsTable extends Migration
      */
     public function up()
     {
-        Schema::create('meal_breakdowns', function (Blueprint $table) {
-            $table->id();
-            $table->enum('diet_type', ['traditional', 'keto']);
-            $table->bigInteger('total_calories')->default(0);
-            $table->unsignedBigInteger('breakfast_units')->default(0);
-            $table->unsignedBigInteger('lunch_units')->default(0);
-            $table->unsignedBigInteger('dinner_units')->default(0);
-            $table->unsignedBigInteger('fruit_units')->default(0);
-            $table->unsignedBigInteger('snack_units')->default(0);
-            $table->timestamps();
-            $table->softDeletes();
+        if(!Schema::hasTable('meal_breakdowns')) {
+            Schema::create('meal_breakdowns', function (Blueprint $table) {
+                $table->id();
+                $table->enum('diet_type', ['traditional', 'keto']);
+                $table->bigInteger('total_calories')->default(0);
+                $table->unsignedBigInteger('breakfast_units')->default(0);
+                $table->unsignedBigInteger('lunch_units')->default(0);
+                $table->unsignedBigInteger('dinner_units')->default(0);
+                $table->unsignedBigInteger('fruit_units')->default(0);
+                $table->unsignedBigInteger('snack_units')->default(0);
+                $table->timestamps();
+                $table->softDeletes();
 
-            $table->unique(['diet_type', 'total_calories']);
-        });
+                $table->unique(['diet_type', 'total_calories']);
+            });
+        }
     }
 
     /**
--- database/migrations/2024_07_18_185137_create_nplan_day_recipe_ingredients_table.php
+++ database/migrations/2024_07_18_185137_create_nplan_day_recipe_ingredients_table.php
@@ -13,20 +13,22 @@ class CreateNplanDayRecipeIngredientsTable extends Migration
      */
     public function up()
     {
-        Schema::create('nplan_day_recipe_ingredients', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('nplan_day_recipe_id');
-            $table->enum('type', ['main', 'sub']);
-            $table->text('name');
-            $table->bigInteger('quantity');
-            $table->enum('unit', ['tsp', 'tbsp', 'fl oz', 'cup', 'pt', 'qt', 'gal', 'ml', 'l', 'oz', 'lb', 'g', 'kg']);
-            $table->bigInteger('scaled_quantity');
-            $table->enum('scaled_unit', ['tsp', 'tbsp', 'fl oz', 'cup', 'pt', 'qt', 'gal', 'ml', 'l', 'oz', 'lb', 'g', 'kg']);
-            $table->timestamps();
-            $table->softDeletes();
+        if (!Schema::hasTable('nplan_day_recipe_ingredients')) {
+            Schema::create('nplan_day_recipe_ingredients', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('nplan_day_recipe_id');
+                $table->enum('type', ['main', 'sub']);
+                $table->text('name');
+                $table->bigInteger('quantity');
+                $table->enum('unit', ['tsp', 'tbsp', 'fl oz', 'cup', 'pt', 'qt', 'gal', 'ml', 'l', 'oz', 'lb', 'g', 'kg']);
+                $table->bigInteger('scaled_quantity');
+                $table->enum('scaled_unit', ['tsp', 'tbsp', 'fl oz', 'cup', 'pt', 'qt', 'gal', 'ml', 'l', 'oz', 'lb', 'g', 'kg']);
+                $table->timestamps();
+                $table->softDeletes();
 
-            $table->foreign('nplan_day_recipe_id')->references('id')->on('nutrition_plan_day_recipes')->onDelete('cascade');
-        });
+                $table->foreign('nplan_day_recipe_id')->references('id')->on('nutrition_plan_day_recipes')->onDelete('cascade');
+            });
+        }
     }
 
     /**

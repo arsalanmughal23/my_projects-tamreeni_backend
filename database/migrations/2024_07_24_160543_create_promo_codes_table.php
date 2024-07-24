59e9b8585f09a99302a469552da839149ab8ed3b
Arsalan Mughal <arsalan.mughal@tekrevol.com>
2024-07-24 21:48:51 +0500

    [ADDED] Make Migration for Promo-Code Module

---
+++ database/migrations/2024_07_24_160543_create_promo_codes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromoCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promo_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->decimal('value', 8, 2, true);
+            $table->enum('type', ['flat', 'percent'])->nullable(false);
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promo_codes');
+    }
+}

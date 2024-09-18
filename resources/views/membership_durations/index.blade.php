5714e3fef8c4f8d11d1b32cc421ec6d04f543e7a
Arsalan Mughal <arsalan.mughal@tekrevol.com>
2024-09-18 19:04:42 +0500

    fix Membership, MembershipDuration, UserMembership, PromoCode, UsedPromoCode Modules

--- resources/views/membership_durations/index.blade.php
+++ resources/views/membership_durations/index.blade.php
@@ -26,14 +26,8 @@
         <div class="clearfix"></div>
 
         <div class="card">
-            <div class="card-body p-0">
+            <div class="card-body">
                 @include('membership_durations.table')
-
-                <div class="card-footer clearfix">
-                    <div class="float-right">
-                        
-                    </div>
-                </div>
             </div>
 
         </div>
